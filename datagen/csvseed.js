3c24cd3cc75b5aa324976e75e71107468457d492
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-04-18 15:56:00 -0700

    Adds updated seed file. Currently seed file is maxing out at 10,000 records generated

--- datagen/csvseed.js
+++ datagen/csvseed.js
@@ -2,40 +2,22 @@ const fs = require('fs');
 const faker = require('faker');
 
 //Data gen settings
-let numberOfRows = 100;
-let fileName = 'test1.csv';
-
-let i = 0;
-let houseId = 1;
+let numberOfRows = 100000;
+let fileName = 'test2.csv';
+let i = 1;
 let agentType = 'premier';
 
 while (i < numberOfRows + 1) {
-    if (i === 0) {
-        fs.appendFile(fileName, `houseId, name, agentType, reviews, recentSales, phone, url\n`, (err) => {
-            if (err) {
-                console.log('error creating or writing to file on iteration ' + i, err);
-            }
-        });
 
-    } else {
-
-        if (i % 4 === 0) {
-            agentType = 'listing';
-        } else {
-            agentType = "premier";
+    fs.appendFile(
+        fileName, 
+        `${faker.name.findName()}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`, 
+        (err) => {
+        if (err) {
+            console.log('error creating or writing to file on iteration ' + i, err);
         }
+    });
 
-        fs.appendFile('test1.csv', `${houseId}, ${faker.name.findName()}, ${agentType}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`, (err) => {
-            if (err) {
-                console.log('error creating or writing to file on iteration ' + i, err);
-            }
-        });
-
-        if (i % 4 === 0) {
-            houseId++;
-        }
-
-    }
     i++;
 }
 
