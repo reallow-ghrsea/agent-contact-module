26b7526cec20a6bb124bf38562941d2b3bfb1d87
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-04-19 16:20:50 -0700

    Adds seeding script which can seed 10M rows

--- datagen/csvseed.js
+++ datagen/csvseed.js
@@ -1,22 +1,31 @@
 const fs = require('fs');
 const faker = require('faker');
-const stream = fs.createWriteStream('./agent_info.csv');
+const writeable = fs.createWriteStream('./agents.csv');
 
-//Data gen settings
-let numberOfRows = 1000000;
-
-let i = 1;
-let agentType = 'premier';
-
-while (i < numberOfRows + 1) {
-
-    stream.write(
-        `${faker.name.findName()}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`
-        );
-
-    i++;
+function getRow () {
+    return `${faker.name.findName()}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`;
 }
 
-stream.end();
+function writeRows (writer) {
+    let numberOfRows = 10000000;
+    function writing () {
+        let flowing = true;
+        do {
+            numberOfRows--;
+            if (numberOfRows === 0) {
+                writer.write(getRow());
+            } else {
+                flowing = writer.write(getRow());
+            }
+        } while (numberOfRows > 0 && flowing);
+        if (numberOfRows > 0) {
+            writer.once('drain', writing);
+        }
+    }
+
+    writing();
+}
+
+writeRows(writeable);
 
 console.log("Done with writing sample data to agent_info.csv");
