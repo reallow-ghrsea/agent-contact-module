9a5ca22096d5503797b519b0e278bb3831cdde0f
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-04-18 20:15:28 -0700

    Adds seed script which can seed 1 million records at a time. However this script causes an out of memory error when trying to seed 10 million rows.

--- datagen/csvseed.js
+++ datagen/csvseed.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
 const faker = require('faker');
+const stream = fs.createWriteStream('./agent_info.csv');
 
 //Data gen settings
-let numberOfRows = 100000;
-let fileName = 'test2.csv';
+let numberOfRows = 1000000;
+
 let i = 1;
 let agentType = 'premier';
 
 while (i < numberOfRows + 1) {
 
-    fs.appendFile(
-        fileName, 
-        `${faker.name.findName()}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`, 
-        (err) => {
-        if (err) {
-            console.log('error creating or writing to file on iteration ' + i, err);
-        }
-    });
+    stream.write(
+        `${faker.name.findName()}, ${faker.random.number(100)}, ${faker.random.number(80)}, ${faker.phone.phoneNumberFormat(1)}, ${faker.image.avatar()}\n`
+        );
 
     i++;
 }
 
-console.log("Done with writing sample data to " + fileName);
+stream.end();
+
+console.log("Done with writing sample data to agent_info.csv");
