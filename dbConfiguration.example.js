130beca5d5af096dba29f84b05d96bf768a66c85
Rachael Otto <rachaelotto@Rachaels-MacBook-Pro.local>
2019-05-01 17:33:52 -0700

    Cleans up files fro deployment

---
+++ dbConfiguration.example.js
@@ -0,0 +1,9 @@
+let options = {
+    user: "",
+    host: "",
+    database: "",
+    password: "",
+    port: 0
+};
+
+module.exports = options;
--- dbConfiguration.js
+++
@@ -1,9 +0,0 @@
-let options = {
-  // host: 'agents3.csast8i0snkv.us-east-2.rds.amazonaws.com',
-  user: "root",
-  // password: 'Password1!',
-  database: "agents"
-  //this is a really obvious change
-};
-
-module.exports = options;
